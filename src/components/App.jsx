dfb1c0ee051819b947e7321f09b868149ac9c762
Franco Abaroa <franco9395@gmail.com>
2016-09-30 14:19:05 -0700

    Minor changes Mj#

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,9 @@ var App = () => (
   </div>
 );
 
+//hello
+//how are you
+//great thank you
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
