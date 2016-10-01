363bd314945e760bdeda1fdec5b09197ea02aa98
francoabaroa <franco9395@gmail.com>
2016-10-01 12:42:12 -0700

    Minor tweaks, working on GET request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,18 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.onVideoClick = this.onVideoClick.bind(this);
+    this.formSubmit = this.formSubmit.bind(this);
     this.state = {
       videoList: exampleVideoData,
       currentVideo: exampleVideoData[0],
       done: false
     };
-    console.log(this.color);
   }
-
-  //add clickhandlerfunction here
-    //make sure to bind this correctly
   
+  formSubmit() {
+    var searchInput = $('.form-control').val();
+    console.log(searchInput);
+  }
 
   onVideoClick(data) {
     console.log(this, data);
@@ -32,7 +33,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Nav />
+        <Nav {...this.props} {...this.state} onClick={this.formSubmit}/>
         <div className="col-md-7">
           <VideoPlayer {...this.props} {...this.state} video={this.state.currentVideo} onClick={this.onVideoClick}/>
         </div>
@@ -42,12 +43,7 @@ class App extends React.Component {
       </div>
     );
   }
-
-  handleClick() {
-    console.log(this);
-  }
 }
-
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -2,10 +2,21 @@ var searchYouTube = (options, callback) => {
   return $.ajax({
     url: 'https://www.googleapis.com/youtube/v3/search',
     type: 'GET',
-    data: {limit: options.max},
+    data: {maxResults: options.max, q: options.query, access_token: options.key, part: 'snippet'},
     success: (x) => callback === undefined ? x : callback(x),
-    error: () => console.log('youtube search request failed')
+    error: (x) => console.log('youtube search request failed: ', x)
   });
 };
 
+console.log(searchYouTube({max: 5, query: 'test', key: window.YOUTUBE_API_KEY}, function () {
+  console.log('success');
+}));
+
+
 window.searchYouTube = searchYouTube;
+
+
+// var request = gapi.client.youtube.search.list({
+//     q: q,
+//     part: 'snippet'
+//   });
